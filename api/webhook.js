7819434a8b3c97dc375178626d5f2ce51e9d592d
EricLin0905 <rivenslover@gmail.com>
2025-09-17 22:45:51 +0800

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
-    return res.status(200).json({ ok: true, received: req.body });
+    return res.status(200).json({ ok: true, body: req.body });
   }
-  res.status(405).json({ error: "Method Not Allowed" });
+  res.status(405).end(); // Method Not Allowed
 }
